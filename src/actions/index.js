111dafe37395c536f502d1fc5924737620120460
pitjack <pitiousplasma@gmail.com>
2017-08-17 00:29:47 +0700

    delete all for edit structure

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './NewsAction'
-export * from './EventsAction'
-export * from './CareersAction'
-export * from './DonatesAction'
-export * from './AlumniAction'
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import thunk from 'redux-thunk'
-import { createStore, compose, applyMiddleware } from 'redux'
-import reducer from '../reducers'
-
-exports.configureStore = () => {
-    let store = createStore(reducer, applyMiddleware(thunk))
-
-    return store
-}
